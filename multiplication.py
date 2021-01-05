3fe927194c1236806ae124301a64b21ca7fe878c
bjknbrrr <57438425+bjknbrrr@users.noreply.github.com>
2021-01-05 19:28:07 +0200

    Update multiplication.py

--- multiplication.py
+++ multiplication.py
@@ -1,23 +1,12 @@
 a = int(input('Please, enter a number: '))
 b = int(input('Please, enter a number: '))
 c = a
-list1 = []
 def mult(a, b, c, list1):
-
-
     for i in range(1, b):
-        list1.append(i)
-#print(list1)
-
-
-    for element in list1:
-        if element <= b:
+        if i <= b:
             a += c
-#        print(a)
     if a == 0 or b == 0:
         return('0')
     else:
         return(a)
-
-
 print(mult(a, b, c, list1))
