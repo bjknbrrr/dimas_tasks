5511f36d38da6dfc1f33bd5a7c6b1ae3231eab63
bjknbrrr <57438425+bjknbrrr@users.noreply.github.com>
2021-01-09 21:39:41 +0200

    Update multiplication.py

--- multiplication.py
+++ multiplication.py
@@ -4,14 +4,19 @@ b = int(input('Please, enter a number: '))
 def mult(a, b):
     c = a
     if a == 0 or b == 0:
-        return('0')
+        return(0)
 
     if b < 0:
         b = -b
+        for i in range(1, b):
+            if i <= b:
+                a += c
+        return (-a)
 
     for i in range(1, b):
         if i <= b:
             a += c
+
     return(a)
 
 print(mult(a, b))
