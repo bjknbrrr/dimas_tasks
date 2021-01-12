de3157f74cec14c8bc15d8e7ed2d07e6e795d5c3
bjknbrrr <57438425+bjknbrrr@users.noreply.github.com>
2021-01-12 19:51:19 +0200

    Update fuctorial.py

--- fuctorial.py
+++ fuctorial.py
@@ -1,7 +1,16 @@
+#ЦИКЛ
+
 def fuctorial(a):
     res = 1
     for i in range(1, a + 1):
         res *= i
     return(res)
-
 print(fuctorial(5))
+
+#РЕКУРСИЯ
+def fuctorial(a, res):
+    if a == 0 or a==1:
+        return res
+    else:
+        return fuctorial(a-1, (res*(a)))
+print(fuctorial(6, 1))
