6b6a55b4ffe0c7265419090c8278a075878daf35
bjknbrrr <57438425+bjknbrrr@users.noreply.github.com>
2021-01-28 13:03:16 +0200

    Update mass.py

--- mass.py
+++ mass.py
@@ -1,13 +1,18 @@
 a = [5, 1, 17, 6, 8, 34, 19, 9]
 a.sort()
-def sort_f(a):
+b = []
+
+def sort_even(a):
     for i in a:
-        if i % 2 == 1:
-            print(i)
+        if i % 2 == 0:
+            b.append(i)
+    return b
+
+def sort_odd(a):
     for j in a:
-        if j % 2 == 0:
-            print(j)
-sort_f(a)
+        if j % 2 == 1:
+            b.append(j)
+    return b
 
-
-
+print(sort_odd(a))
+print(sort_even(a))
